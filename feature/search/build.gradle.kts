b2e2885a58bb0ff554925c3aae6fa6f0305fe241
hiroki.horiguchi <hiroki.horiguchi.private@gmail.com>
2024-10-29 18:20:05 +0900

    必要なライブラリを追加

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -45,9 +45,17 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(project(":core:domain"))
+    implementation(project(":core:network"))
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.foundation)
 
     // runtime-utility
     implementation(libs.androidx.lifecycle.utility)
